ea6847e64e3f6ec6d9b5530c850854958ea64a1c
Ayush Dewangan <ayush.dewangan2109@gmail.com>
2025-04-11 23:29:20 +0530

    Added my-folder with .gitkeep

---
+++ src/App.jsx
@@ -0,0 +1,341 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  // document.getElementById("#div017").ontouchmove
+
+  return (
+    <div id="div00">
+      <div id="div01">Free Shipping On Order Over $75. Easy Returns.</div>
+      <div id="div02">
+        <div id="div03">
+          <div className="underhover">MEN</div>
+          <div className="underhover">WOMEN</div>
+          <div className="underhover">SOCKS</div>
+          <div className="underhover">NEW ARRIVALS</div>
+        </div>
+        <div id="div04"></div>
+        <div id="div05">
+          <div id="div06" className="underhover">
+            sustainability
+          </div>
+          <div id="div07" className="underhover">
+            return
+          </div>
+          <div id="div08" className="underhover">
+            stores
+          </div>
+          <div id="icons"></div>
+          <div id="icont"></div>
+          <div id="iconp"></div>
+          <div id="iconq"></div>
+        </div>
+      </div>
+      <div id="div09">
+        <div className="underhover">Men's Shoes</div>
+        <div className="underhover">Women's Shoes</div>
+        <div className="underhover">New Arrivals</div>
+      </div>
+      <div id="div10">
+        <div id="div011">
+          <div id="text01">Bring It, Spring.</div>
+          <div id="text02">
+            The extra-durable Utility Collection is up for everything.
+          </div>
+          <div id="div015">
+            <div id="div012">SHOP MEN</div>
+            <div id="div013">SHOP WOMEN</div>
+          </div>
+        </div>
+      </div>
+      <div id="div016">
+        <div id="div017">
+          <div id="div022">Bestsellers</div>
+          <div id="div023">Fan-Favorite Sneakers, Flats, and Slip-Ons</div>
+          <div id="div020">
+            <div id="div012">SHOP MEN</div>
+            <div id="div013">SHOP WOMEN</div>
+          </div>
+        </div>
+
+        <div id="div018">
+          <div id="div022">New Arrivals</div>
+          <div id="div023">The Latest Styles & Limited-Edition Colors</div>
+          <div id="div020">
+            <div id="div012">SHOP MEN</div>
+            <div id="div013">SHOP WOMEN</div>
+          </div>
+        </div>
+
+        <div id="div019">
+          <div id="div022">Spring Essentials</div>
+          <div id="div023">
+            Breezy Shoes Made From Tree Fiber For Warmer Days Ahead
+          </div>
+          <div id="div020" className="div020">
+            <div id="div012">SHOP MEN</div>
+            <div id="div013">SHOP WOMEN</div>
+          </div>
+        </div>
+      </div>
+      <div id="div024">
+        <div id="div025">We Make Better Things In A Better Way</div>
+        <div id="div026">
+          By looking to the world’s greatest innovator—Nature—we create shoes
+          that deliver unrivaled comfort that you feel good in and feel good
+          about.
+        </div>
+        <div id="div027"></div>
+        <div style={{ color: "white", fontSize: "1.5vh" }}>BY NATURE</div>
+      </div>
+      <div id="div028">
+        <div id="div029">
+          <div id="div035">Rock And Stroll In Comfort</div>
+          <div id="div036">
+            Effortlessly comfy styles take color cues from the Southwest’s
+            rustic rock formations.
+          </div>
+          <div id="div037">
+            <div id="div038">SHOP MEN</div>
+            <div id="div038">SHOP WOMEN</div>
+          </div>
+        </div>
+        <div id="div030">
+          <div id="div031">
+            <div id="div040">
+              <div id="div0421">SHOP MEN</div>
+              <div id="div0422">SHOP WOMEN</div>
+            </div>
+            <div id="div041">
+              <div id="div043">
+                <div id="div04501">Tree Dasher 2</div>
+                <div id="div04502">Rustic Orage</div>
+              </div>
+              <div id="div044">$135</div>
+            </div>
+          </div>
+          <div id="div032">
+            <div id="div040">
+              <div id="div0421">SHOP MEN</div>
+              <div id="div0422">SHOP WOMEN</div>
+            </div>
+            <div id="div041">
+              <div id="div043">
+                <div id="div04501">Tree Runner Go</div>
+                <div id="div04502">Nature White/Rustic Orage</div>
+              </div>
+              <div id="div044">$120</div>
+            </div>
+          </div>
+          <div id="div033">
+            <div id="div040">
+              <div id="div0421">SHOP MEN</div>
+              <div id="div0422">SHOP WOMEN</div>
+            </div>
+            <div id="div041">
+              <div id="div043">
+                <div id="div04501">Tree Gliders</div>
+                <div id="div04502">Twilight White/Rustic Orange</div>
+              </div>
+              <div id="div044">$135</div>
+            </div>
+          </div>
+          <div id="div034">
+            <div id="div040">
+              <div id="div0421">SHOP MEN</div>
+              <div id="div0422">SHOP WOMEN</div>
+            </div>
+            <div id="div041">
+              <div id="div043">
+                <div id="div04501">Tree Runner</div>
+                <div id="div04502">Rustic Orage</div>
+              </div>
+              <div id="div044">$98</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="div046">
+        <div id="div047">
+          <div id="div049">Cards On The Table</div>
+          <div id="div050"> Hosted by Stanley Tucci</div>
+          <div id="div051">WATCH NOW</div>
+        </div>
+
+        <div id="div048">
+          <div id="div049">Canvas Piper</div>
+          <div id="div050">Versatile Style, Durable Design</div>
+          <div id="div051">SHOP</div>
+        </div>
+      </div>
+
+      <div className="container swiper">
+        <div id="div0001">MORE TO SHOP</div>
+        <div className="slider-wrapper">
+          <div className="card-list swiper-wrapper">
+            <div>
+              <div className="card-item swiper-slide" id="cardimg001"></div>
+              <div id="div053">Women's Tree Runner Go - Utility</div>
+              <div id="div054">Blizzard/Deep Navy</div>
+              <div id="div053">$130</div>
+            </div>
+
+            <div>
+              <div className="card-item swiper-slide" id="cardimg002"></div>
+              <div id="div053">Men's Tree Dasher 2 - Utility</div>
+              <div id="div054">Natural Black</div>
+              <div id="div053">$145</div>
+            </div>
+            <div>
+              <div className="card-item swiper-slide" id="cardimg003"></div>
+              <div id="div053">Women's Canvas Pipers</div>
+              <div id="div054">Blizzard</div>
+              <div id="div053">$90</div>
+            </div>
+            <div>
+              <div className="card-item swiper-slide" id="cardimg004"></div>
+              <div id="div053">Men's Tree Runner</div>
+              <div id="div054">Rustic Orange</div>
+              <div id="div053">$98</div>
+            </div>
+
+            <div>
+              <div className="card-item swiper-slide" id="cardimg005"></div>
+              <div id="div053">Men's Tree Runner Go</div>
+              <div id="div054">Natural White/Rustic Orannge</div>
+              <div id="div053">$120</div>
+            </div>
+            <div>
+              <div className="card-item swiper-slide" id="cardimg006"></div>
+              <div id="div053">Women's Tree Breezers</div>
+              <div id="div054">Light Khakhi</div>
+              <div id="div053">$100</div>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+
+      <div id="div055">
+        <div id="div056">Want First Dibs?</div>
+        <div id="div057">
+          Join our email list and be the first to know about new limited edition
+          products, material innovations, and lots of other fun updates.
+        </div>
+        <div id="div058">
+          <input id="d0581" placeholder="Enter Your Email Address"></input>
+          <div id="d0582">SIGN UP</div>
+        </div>
+        <div id="div059">
+          Note: You can opt-out at any time. See our{" "}
+          <u>
+            {" "}
+            <strong> Privacy Policy</strong>
+          </u>{" "}
+          and{" "}
+          <u>
+            <strong>Terms</strong>
+          </u>
+          .
+        </div>
+      </div>
+
+      <div id="div060">
+        <div id="div061">The Allbirds Approach</div>
+        <div id="div062">
+          <div id="div063">
+            <div id="div064">Wear-All-Day Comfort</div>
+            <div id="div065">
+              Lightweight, bouncy, and wildly comfortable, Allbirds shoes make
+              any outing feel effortless. Slip in, lace up, or slide them on and
+              enjoy the comfy support. <u> Learn More</u>.
+            </div>
+          </div>
+          <div id="div063">
+            <div id="div064">Sustainability In Every Step</div>
+            <div id="div065">
+              From materials to transport, we’re working to reduce our carbon
+              footprint to near zero. Holding ourselves accountable and striving
+              for climate goals isn’t a 30-year goal—it’s now.{" "}
+              <u> Learn More</u>.
+            </div>
+          </div>
+          <div id="div063">
+            <div id="div064">Materials From The Earth</div>
+            <div id="div065">
+              We replace petroleum-based synthetics with natural alternatives
+              wherever we can. Like using wool, tree fiber, and sugar cane.
+              They’re soft, breathable, and better for the planet—win, win, win.
+              <u> Learn More</u>.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="div066">
+        <div id="div067">
+          <div id="div068">
+            <ul>HELP</ul>
+            <ul>Chat with us!</ul>
+            <ul>Returns/Exchanges</ul>
+            <ul>FAQ/Contact Us</ul>
+            <ul>Afterpay</ul>
+          </div>
+          <div id="div068">
+            <ul>SHOP</ul>
+            <ul>Men's Shoes</ul>
+            <ul>Women's Shoes</ul>
+            <ul>Men's Apparel</ul>
+            <ul>Women's Apparel</ul>
+            <ul>Socks</ul>
+            <ul>Gift Cards</ul>
+            <ul>Refer a Friend</ul>
+          </div>
+          <div id="div067">
+            <div id="div068">
+              <ul>COMPANY</ul>
+              <ul>Our Stores</ul>
+              <ul>Our Story</ul>
+              <ul>Our Materials</ul>
+              <ul>Sustainability</ul>
+              <ul>Investors</ul>
+              <ul>Shoe Care</ul>
+              <ul>Affiliates</ul>
+              <ul>Bulk Orders</ul>
+              <ul>Careers</ul>
+              <ul>Press</ul>
+
+              <ul>California Transparency Act</ul>
+              <ul>Community Offers</ul>
+              <ul>Our Blog</ul>
+              <ul>Our Blog</ul>
+            </div>
+          </div>
+        </div>
+
+        <div id="div070">
+          <div id="div069">
+            This website is inspired by{" "}
+            <a
+              id="div071"
+              href="https://www.allbirds.com/?srsltid=AfmBOooePR6EGX84vHec1qTB_x9WPV4Ha3Bix_n7IoaLmu3aBKl0M_Vq"
+            >
+              allbirds.com
+            </a>
+          </div>
+          <div>Developed by : Ayush Dewangan</div>
+          <div>Email : ayush.dewangan2109@gmail.com</div>
+          <div>
+            Linkedin :{" "}
+            <a id="div071" href="https://www.linkedin.com/in/ayushdewangan21/">
+              Ayush Dewangan
+            </a>
+          </div>
+        </div>
+      </div>
+      <div id="end"></div>
+    </div>
+  );
+};
+
+export default App;
